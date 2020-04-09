cbb518920168663037015c77610602dbfedf80fa
bcikota <bcikota@gmail.com>
2020-04-09 17:56:02 +0200

    DRY code > js

--- js/game.js
+++ js/game.js
@@ -1,5 +1,3 @@
-//dohvati sva polja + iteracija + event listener
-
 let items = document.querySelectorAll('.item');
 let count = 0;
 let a1 = document.querySelector('#a1');
@@ -17,7 +15,8 @@ let playAgain = document.querySelector('.play-again');
 let gameOver = false;
 
 items.forEach(item => {
-    item.addEventListener('click', function () {
+
+    function game() {
 
         const counterCreator = () => {
             return () => {
@@ -27,62 +26,62 @@ items.forEach(item => {
 
         const counter = counterCreator();
         counter();
-        
-        if (count % 2 === 1 && gameOver === false && item.textContent === '') {
-            item.textContent = 'X';
-        } else if(count % 2 === 0 && gameOver === false && item.textContent === '' ) {
-            item.textContent = 'O';
-        } else {
-            count--;
+
+        function writeXorO() {
+            if (count % 2 === 1 && gameOver === false && item.textContent === '') {
+                item.textContent = 'X';
+            } else if (count % 2 === 0 && gameOver === false && item.textContent === '') {
+                item.textContent = 'O';
+            } else {
+                count--;
+            }
         }
 
-        if (a1.textContent === a2.textContent && a2.textContent === a3.textContent && a1.textContent !== '' && a2.textContent !== '' && a3.textContent !== '' ) {
+        writeXorO();
+
+        function cssAndGameOver() {
             hr.style.display = 'block';
-            hr.classList.add('hr-a1-a3');
             container.style.marginTop = '3rem';
             gameOver = true;
-        } else if (a1.textContent === b2.textContent && b2.textContent === c3.textContent && a1.textContent !== '' && b2.textContent !== '' && c3.textContent !== ''){
-            hr.style.display = 'block';
-            hr.classList.add('hr-a1-c3');
-            container.style.marginTop = '3rem';
-            gameOver = true;
-        } else if (a3.textContent === b2.textContent && b2.textContent === c1.textContent && a3.textContent !== '' && b2.textContent !== '' && c1.textContent !== '') {
-            hr.style.display = 'block';
-            hr.classList.add('hr-a3-c1');
-            container.style.marginTop = '3rem';
-            gameOver = true;
-        } else if (b1.textContent === b2.textContent && b2.textContent === b3.textContent && b1.textContent !== '' && b2.textContent !== '' && b3.textContent !== '') {
-            hr.style.display = 'block';
-            hr.classList.add('hr-b1-b3');
-            container.style.marginTop = '3rem';
-            gameOver = true;
-        } else if (c1.textContent === c2.textContent && c2.textContent === c3.textContent && c1.textContent !== '' && c2.textContent !== '' && c3.textContent !== '') {
-            hr.style.display = 'block';
-            hr.classList.add('hr-c1-c3');
-            container.style.marginTop = '3rem';
-            gameOver = true;
-        } else if (a1.textContent === b1.textContent && b1.textContent === c1.textContent && a1.textContent !== '' && b1.textContent !== '' && c1.textContent !== ''){
-            hr.style.display = 'block';
-            hr.classList.add('hr-a1-c1');
-            container.style.marginTop = '3rem';
-            gameOver = true;
-        } else if (a2.textContent === b2.textContent && b2.textContent === c2.textContent && a2.textContent !== '' && b2.textContent !== '' && c2.textContent !== ''){
-            hr.style.display = 'block';
-            hr.classList.add('hr-a2-c2');
-            container.style.marginTop = '3rem';
-            gameOver = true;
-        } else if (a3.textContent === b3.textContent && b3.textContent === c3.textContent && a3.textContent !== '' && b3.textContent !== '' && c3.textContent !== ''){
-            hr.style.display = 'block';
-            hr.classList.add('hr-a3-c3');
-            container.style.marginTop = '3rem';
-            gameOver = true;
-        } 
-        
-    });
+        }
+
+
+        function drawLine() {
+            if (a1.textContent === a2.textContent && a2.textContent === a3.textContent && a1.textContent !== '' && a2.textContent !== '' && a3.textContent !== '') {
+                hr.classList.add('hr-a1-a3');
+                cssAndGameOver();
+            } else if (a1.textContent === b2.textContent && b2.textContent === c3.textContent && a1.textContent !== '' && b2.textContent !== '' && c3.textContent !== '') {
+                hr.classList.add('hr-a1-c3');
+                cssAndGameOver();
+            } else if (a3.textContent === b2.textContent && b2.textContent === c1.textContent && a3.textContent !== '' && b2.textContent !== '' && c1.textContent !== '') {
+                hr.classList.add('hr-a3-c1');
+                cssAndGameOver();
+            } else if (b1.textContent === b2.textContent && b2.textContent === b3.textContent && b1.textContent !== '' && b2.textContent !== '' && b3.textContent !== '') {
+                hr.classList.add('hr-b1-b3');
+                cssAndGameOver();
+            } else if (c1.textContent === c2.textContent && c2.textContent === c3.textContent && c1.textContent !== '' && c2.textContent !== '' && c3.textContent !== '') {
+                hr.classList.add('hr-c1-c3');
+                cssAndGameOver();
+            } else if (a1.textContent === b1.textContent && b1.textContent === c1.textContent && a1.textContent !== '' && b1.textContent !== '' && c1.textContent !== '') {
+                hr.classList.add('hr-a1-c1');
+                cssAndGameOver();
+            } else if (a2.textContent === b2.textContent && b2.textContent === c2.textContent && a2.textContent !== '' && b2.textContent !== '' && c2.textContent !== '') {
+                hr.classList.add('hr-a2-c2');
+                cssAndGameOver();
+            } else if (a3.textContent === b3.textContent && b3.textContent === c3.textContent && a3.textContent !== '' && b3.textContent !== '' && c3.textContent !== '') {
+                hr.classList.add('hr-a3-c3');
+                cssAndGameOver();
+            }
+        }
+
+        drawLine();
+    }
+
+    item.addEventListener('click', game);
 
 });
 
-playAgain.addEventListener('click', function(){
+playAgain.addEventListener('click', function () {
     window.location.reload(true);
 });
 
