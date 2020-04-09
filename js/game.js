f85f3f2a413a2e422acc395f003722eb7a504dbf
bcikota <bcikota@gmail.com>
2020-04-09 13:52:05 +0200

    css in js correction

--- js/game.js
+++ js/game.js
@@ -39,7 +39,7 @@ items.forEach(item => {
         if (a1.textContent === a2.textContent && a2.textContent === a3.textContent && a1.textContent !== '' && a2.textContent !== '' && a3.textContent !== '' ) {
             hr.style.display = 'block';
             hr.classList.add('hr-a1-a3');
-            container.style.marginTop = '2rem';
+            container.style.marginTop = '3rem';
             gameOver = true;
         } else if (a1.textContent === b2.textContent && b2.textContent === c3.textContent && a1.textContent !== '' && b2.textContent !== '' && c3.textContent !== ''){
             hr.style.display = 'block';
@@ -54,27 +54,27 @@ items.forEach(item => {
         } else if (b1.textContent === b2.textContent && b2.textContent === b3.textContent && b1.textContent !== '' && b2.textContent !== '' && b3.textContent !== '') {
             hr.style.display = 'block';
             hr.classList.add('hr-b1-b3');
-            container.style.marginTop = '2rem';
+            container.style.marginTop = '3rem';
             gameOver = true;
         } else if (c1.textContent === c2.textContent && c2.textContent === c3.textContent && c1.textContent !== '' && c2.textContent !== '' && c3.textContent !== '') {
             hr.style.display = 'block';
             hr.classList.add('hr-c1-c3');
-            container.style.marginTop = '2rem';
+            container.style.marginTop = '3rem';
             gameOver = true;
         } else if (a1.textContent === b1.textContent && b1.textContent === c1.textContent && a1.textContent !== '' && b1.textContent !== '' && c1.textContent !== ''){
             hr.style.display = 'block';
             hr.classList.add('hr-a1-c1');
-            container.style.marginTop = '2rem';
+            container.style.marginTop = '3rem';
             gameOver = true;
         } else if (a2.textContent === b2.textContent && b2.textContent === c2.textContent && a2.textContent !== '' && b2.textContent !== '' && c2.textContent !== ''){
             hr.style.display = 'block';
             hr.classList.add('hr-a2-c2');
-            container.style.marginTop = '2rem';
+            container.style.marginTop = '3rem';
             gameOver = true;
         } else if (a3.textContent === b3.textContent && b3.textContent === c3.textContent && a3.textContent !== '' && b3.textContent !== '' && c3.textContent !== ''){
             hr.style.display = 'block';
             hr.classList.add('hr-a3-c3');
-            container.style.marginTop = '2rem';
+            container.style.marginTop = '3rem';
             gameOver = true;
         } 
         
